e0a61a04114737dfa368926639a336cb250d82a0
kyjun14 <kyjun14@naver.com>
2023-08-11 15:14:15 +0900

    20230811

--- Project/infra/ec2/ami/main.tf
+++ Project/infra/ec2/ami/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ami_from_instance" "project01_target_ami" {
     name = "project01_target_ami"
-    source_instance_id = "i-0e881d7ae2d77ec12"
+    source_instance_id = [data.terraform_remote_state.project01_target.outputs.instance_id]
     snapshot_without_reboot = true
 
     tags = {
