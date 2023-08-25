5d22d44a2051b06f68b66ab2ade00b669c6276dc
kyjun14 <kyjun14@naver.com>
2023-08-25 12:05:08 +0900

    20230825

--- Project/infra/ec2/target/install.sh
+++ Project/infra/ec2/target/install.sh
@@ -17,3 +17,7 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
